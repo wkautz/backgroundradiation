7a7c39a53577818a11c11dafafdeecef1e60a44a
Dillon Reece Franke <dfranke@stanford.edu>
2018-11-15 01:51:02 -0800

    Added support for layer analysis, can now determine IPSrc/Dst

--- go/src/pcap/pcap.go
+++ go/src/pcap/pcap.go
@@ -8,11 +8,13 @@ import (
     "fmt"
     "github.com/google/gopacket"
     "github.com/google/gopacket/pcap"
+    "github.com/google/gopacket/layers"
     "log"
 )
 
 var (
-    pcapFile string = "test.pcap"
+    //pcapFile string = "/Volumes/SANDISK256/PCap_Data/2018-10-30.00.pcap"
+    pcapFile string = "/Volumes/SANDISK256/PCap_Data/2018-10-30.00.pcap"
     handle   *pcap.Handle
     err      error
 )
@@ -25,7 +27,35 @@ func main() {
 
     // Loop through packets in file
     packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+    //var(i int = 0)
     for packet := range packetSource.Packets() {
-        fmt.Println(packet)
+
+        fmt.Println("======PACKET LAYERS======")
+        for _, layer := range packet.Layers() {
+            fmt.Println(layer.LayerType())
+        }
+
+        fmt.Println("=====================")
+
+        //Get IPv4 Layer
+        ipLayer := packet.Layer(layers.LayerTypeIPv4)
+        if ipLayer != nil {
+            fmt.Println("IPv4 Layer Detected.")
+            ip, _ := ipLayer.(*layers.IPv4)
+
+            //IP layer variables:
+            //Version (Either 4 or 6)
+            //IHL (IP Header Length in 32-bit words)
+            //TOS, Length, ID, Flages, FragOffset, TTL, Protocol (TCP?, etc.),
+            //Checksum, SrcIP, DstIP
+            fmt.Printf("Source IP: %s\n", ip.SrcIP)
+            fmt.Printf("Destin IP: %s\n", ip.DstIP)
+            fmt.Printf("Protocol: %s\n", ip.Protocol)
+
+            println()
+        }
+
+        //i += 1
+        //if (i == 4) {break}
     }
 }
